bb91666f33b97bf3ab98be92378e8214385a6de0
harrycampaz <harrycampaz@gmail.com>
2020-03-12 21:57:46 -0500

    obervable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
+import { Observable, Observer } from "rxjs";
+
+const observer: Observer<any> = {
+
+    next: value => console.log('Siguiente [Next]', value),
+    error: error => console.warn('Error [obj]', error),
+    complete: () => console.info('Complete [obj]')
+
+};
+
+const obs$ = new Observable<String>( subcriber => {
+
+    subcriber.next('Hola');
+    subcriber.next('mundo');
+
+    // const a = undefined;
+    // a.name = 'Garry'
+
+    subcriber.complete();
 
 
-console.log('Hola Mundo!');
+});
 
 
+// obs$.subscribe(
+//     valor => console.log('next:', valor),
+//     error => console.warn('error:', error),
+//     ()=> console.log('Completado')
+    
+// );
 
 
-
-
-
+obs$.subscribe(observer)
